19f33deb307bd44f1aa272b7cd99e11943ec82d0
Mehmet Köse <mehmet.kose.w6017@student.hogent.be>
2018-03-12 19:40:27 +0100

    ik vergeet te commiten

--- vera/getImages.js
+++ vera/getImages.js
@@ -1,13 +1,14 @@
-var folder = "img/";
+var folder = "images/eski/fullres/";
 
 $.ajax({
     url : folder,
     success: function (data) {
         $(data).find("a").attr("href", function (i, val) {
             if( val.match(/\.(jpe?g|png)$/) ) { 
-                $("body").append( "<img src='" + val +"'>" );
-                
-            } 
+                var final = val.substr(val.lastIndexOf('\\') + 1);
+                var imageHtml = '<a href="images/eski/fullres/' + final + '" data-fancybox="images"><img src="images/eski/320x320/' + final + '" alt="" style="max-width:100%" /></a>'
+                $("#resimler").append(imageHtml)
+            }
         });
     }
 });
